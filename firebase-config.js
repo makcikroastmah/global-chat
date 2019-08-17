4a52855773c13ec39d5fc7e338309698bc6a3d4c
ayman <aymanabdullahjamal@gmail.com>
2019-08-17 12:55:43 +0800

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCayZMlhyyxOCvYnQ5d2YnIbgxJvosYAWk",
+    authDomain: "global-chat-live-54484.firebaseapp.com",
+    databaseURL: "https://global-chat-live-54484.firebaseio.com",
+    projectId: "global-chat-live-54484",
+    storageBucket: "",
+    messagingSenderId: "943677493908",
+    appId: "1:943677493908:web:25ed0668dd2058b9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var db = firebase.firestore();
---
+++ main.js
@@ -0,0 +1,44 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      messages: [
+          {
+            message: "hello",
+            date:new Date(),
+        }
+      ],
+      newMessage: "",
+    },
+    created:function(){
+        db.collection("messages")
+        .orderBy('date','asc')
+        .onSnapshot(messagesCollection => {
+            
+            messagesCollection.forEach(messageItem => {
+                this.messages.push(messagesItem.data());
+            })
+        })
+    },
+    methods: {
+        enterNewMessage: function(){
+            const newMessage = {
+                message: this.newMessage,
+                date: new Date(),
+            }
+           this.messages.push(newMessage);
+           this.addToFirestore(newMessage);
+           this.newMessage = "";
+        },
+        addToFirestore : function(newMessage){
+            db.collection('messages')
+            .add(newMessage)
+            .then (function(documentId){
+                console.log("Document has been inserted with id", documentId)
+            })
+           .catch(function(error){
+            console.log(error);
+           })
+
+        }
+    }
+  })
